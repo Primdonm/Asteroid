7d36a1bec2ae816f12f1dde99080f8becd341901
primdonm <arviscannon@hotmail.com>
2024-12-12 10:16:16 -0500

    finished the player class. player can now rotate and move with wasd. created groups on main for the updates and drawable sections. started the asteroid.py and class

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ def main():
     
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt=0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,7 +27,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+       
         pygame.display.flip()        
         dt = clock.tick(60) / 1000
         #print(f"{dt}")
