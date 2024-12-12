e538ae379a705ca78c1a8b678fe5cbc1e401a9ab
primdonm <arviscannon@hotmail.com>
2024-12-12 11:45:34 -0500

    imported asteroidField.py. added asteroid logic to main.

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from asteroid import Asteroid
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -13,8 +15,11 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -32,6 +37,8 @@ def main():
 
         for obj in drawable:
             obj.draw(screen)
+
+        AsteroidField()
        
         pygame.display.flip()        
         dt = clock.tick(60) / 1000
